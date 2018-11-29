d28487244c4a272899c719575300c7c94e41739a
McLeroy <yoozeey@gmail.com>
2018-11-29 16:49:49 +0100

    initial commit

--- app/src/main/java/com/airviewdevs/androidx/todos/TodoViewModel.java
+++ app/src/main/java/com/airviewdevs/androidx/todos/TodoViewModel.java
@@ -1,5 +1,6 @@
 package com.airviewdevs.androidx.todos;
 
+import com.airviewdevs.androidx.ApiResponse;
 import com.airviewdevs.androidx.App;
 import com.airviewdevs.androidx.api.Resource;
 import com.airviewdevs.androidx.models.Todo;
@@ -8,17 +9,28 @@ import com.airviewdevs.androidx.utils.NetworkResourceLiveData;
 import java.util.List;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
 public class TodoViewModel extends ViewModel {
+    private MediatorLiveData<Resource<Todo>>mediatorLiveData;
     private NetworkResourceLiveData<List<Todo>>todosLiveData;
     public TodoViewModel() {
-
+        mediatorLiveData = new MediatorLiveData<>();
     }
 
     public NetworkResourceLiveData<Resource<List<Todo>>>getTodos(){
         if (todosLiveData == null)
             todosLiveData = new NetworkResourceLiveData<>();
-        App.getApiService().getTodos(1).observe();
+        mediatorLiveData.addSource(App.getApiService().getTodos(1), new Observer<ApiResponse<List<Todo>>>() {
+            @Override
+            public void onChanged(ApiResponse<List<Todo>> listApiResponse) {
+                if (listApiResponse instanceof ApiResponse.ApiSuccessResponse) {
+                    ApiResponse.ApiSuccessResponse successResponse = (ApiResponse.ApiSuccessResponse)listApiResponse;
+                    mediatorLiveData.setValue(Resource.success(successResponse.getBody()));
+                }
+            }
+        });
     }
 }
